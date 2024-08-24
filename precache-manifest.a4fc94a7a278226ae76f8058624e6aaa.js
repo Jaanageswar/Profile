98b9eca2b6680fffb03fb7380e7ef1e198bdc2df
Jaanageswar <jaanageswar2002@gmail.com>
2024-08-24 14:54:20 +0530

    Updates

---
+++ precache-manifest.a4fc94a7a278226ae76f8058624e6aaa.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8185353f56e77495e67b3c4f6d16123",
+    "url": "/Profile/index.html"
+  },
+  {
+    "revision": "488bf4d5e3851ec643b1",
+    "url": "/Profile/static/css/main.3e0357d2.chunk.css"
+  },
+  {
+    "revision": "bfb9475250dab98c528e",
+    "url": "/Profile/static/js/2.3c361976.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/Profile/static/js/2.3c361976.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6232a9942d3df44889f4",
+    "url": "/Profile/static/js/3.f8f792a2.chunk.js"
+  },
+  {
+    "revision": "488bf4d5e3851ec643b1",
+    "url": "/Profile/static/js/main.bdaf5b36.chunk.js"
+  },
+  {
+    "revision": "835b93ee4c90385d7eed",
+    "url": "/Profile/static/js/runtime-main.2a2522a9.js"
+  },
+  {
+    "revision": "3efafee25d5912624e19d1021eab6358",
+    "url": "/Profile/static/media/Jaanageswar_S _CV.3efafee2.pdf"
+  },
+  {
+    "revision": "b6a3775b9e6fb3779ce9062b44ac890e",
+    "url": "/Profile/static/media/call_icon.b6a3775b.svg"
+  },
+  {
+    "revision": "843ce8bcccdf1c34c6d5ba3b6214a22d",
+    "url": "/Profile/static/media/location_icon.843ce8bc.svg"
+  },
+  {
+    "revision": "96d550bf5a72bdb4e401e72be872c6aa",
+    "url": "/Profile/static/media/logo1.96d550bf.svg"
+  },
+  {
+    "revision": "d2bfd99860cc53d7e9c7f2c1ecd93cfe",
+    "url": "/Profile/static/media/mail_icon.d2bfd998.svg"
+  },
+  {
+    "revision": "0e3a06e84834e1eaf28f9b0bb8c16462",
+    "url": "/Profile/static/media/menu_close.0e3a06e8.svg"
+  },
+  {
+    "revision": "7fcad4ea13e04ee0cec0c9b8678a7520",
+    "url": "/Profile/static/media/menu_open.7fcad4ea.svg"
+  },
+  {
+    "revision": "55f92ad3e98a99d9427cb74f0892a3a9",
+    "url": "/Profile/static/media/nav_underline.55f92ad3.svg"
+  },
+  {
+    "revision": "6ee3e48293dd5ec464409017586a4a43",
+    "url": "/Profile/static/media/photo.6ee3e482.png"
+  },
+  {
+    "revision": "22d9073d667072cdc600f50d305244ac",
+    "url": "/Profile/static/media/theme_pattern.22d9073d.svg"
+  },
+  {
+    "revision": "0a3a4bd9d802076fed5ead38ade9c883",
+    "url": "/Profile/static/media/user_icon.0a3a4bd9.svg"
+  }
+]);
